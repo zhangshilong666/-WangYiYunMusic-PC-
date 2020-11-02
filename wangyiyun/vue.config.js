40f0a3644b128c61c21f200c595109d0a69c3dac
zhangshilong123 <1958165915@qq.com>
2020-11-02 14:34:01 +0800

    跨域问题

---
+++ wangyiyun/vue.config.js
@@ -0,0 +1,18 @@
+const proxy = require('http-proxy-middleware');
+module.exports={
+	devServer:{
+		host:"localhost",
+		port:80,
+		proxy:{
+			
+			
+			'http://localhost:3000':{
+				target:"http://172.21.0.17:3000",
+				changeOrigin: true, //是否跨域
+				pathRewrite: { // pathRewrite 的作用是把实际Request Url中的'/api'用""代替
+				'^http://localhost:3000': "/" ,
+				}
+			}
+		}
+	}
+}
