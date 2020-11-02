390aafcd841980ae80637684d9726ec92c1388b0
zhangshilong123 <1958165915@qq.com>
2020-11-02 15:25:00 +0800

    跨域

--- wangyiyun/vue.config.js
+++ wangyiyun/vue.config.js
@@ -4,10 +4,8 @@ module.exports={
 		host:"localhost",
 		port:80,
 		proxy:{
-			
-			
 			'http://localhost:3000':{
-				target:"http://172.21.0.17:3000",
+				target:"http://49.232.11.225:3000",
 				changeOrigin: true, //是否跨域
 				pathRewrite: { // pathRewrite 的作用是把实际Request Url中的'/api'用""代替
 				'^http://localhost:3000': "/" ,
